77b5364fbd2b67ec20736e3161a8ea7daab50138
Jonas Wielicki <j.wielicki@sotecware.net>
2012-02-21 18:02:37 +0100

    * Fixed license header (I think the format now converged to its final version)

--- PythonicUniverse.py
+++ PythonicUniverse.py
@@ -1,37 +1,33 @@
 #!/usr/bin/python2
 # encoding=utf8
+# File name: PythonicUniverse.py
+# This file is part of: pyuni
+# 
+# LICENSE
+# 
+# The contents of this file are subject to the Mozilla Public License
+# Version 1.1 (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+# 
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+# the License for the specific language governing rights and limitations
+# under the License.
+# 
+# Alternatively, the contents of this file may be used under the terms
+# of the GNU General Public license (the  "GPL License"), in which case
+# the provisions of GPL License are applicable instead of those above.
+# 
+# FEEDBACK & QUESTIONS
+# 
+# For feedback and questions about pyuni please e-mail one of the
+# authors named in the AUTHORS file.
+########################################################################
 """
-File name: PythonicUniverse.py
-This file is part of: pyuni
-
-DOCUMENTATION
-
-Note that this part of the License Header can be used for custom
-comments. This and ONLY this part please. If any garbage is in the other
-parts of the license header, it will likely produce garbage on automated
-header generation :).
-
-LICENSE
-
-The contents of this file are subject to the Mozilla Public License
-Version 1.1 (the "License"); you may not use this file except in
-compliance with the License. You may obtain a copy of the License at
-http://www.mozilla.org/MPL/
-
-Software distributed under the License is distributed on an "AS IS"
-basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-License for the specific language governing rights and limitations under
-the License.
-
-Alternatively, the contents of this file may be used under the terms of
-the GNU General Public license (the  "GPL License"), in which case  the
-provisions of GPL License are applicable instead of those above.
-
-FEEDBACK & QUESTIONS
-
-For feedback and questions about pyuni please e-mail one of the authors
-named in the AUTHORS file.
+Nothing yet.
 """
+
 from __future__ import unicode_literals, print_function, division
 from our_future import *
 print("Nothing to see here")
