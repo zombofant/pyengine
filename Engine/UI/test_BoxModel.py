d10f1600d649f86b7db230a168c1f6d9c73844ff
Jonas Wielicki <j.wielicki@sotecware.net>
2012-02-22 23:46:57 +0100

    Further UI work
    * Added more tests for the BaseBox

--- Engine/UI/test_BoxModel.py
+++ Engine/UI/test_BoxModel.py
@@ -1,13 +1,13 @@
 import BoxModel
 import unittest
-import random
+from random import randint
 
-class BoxModelTestCase(unittest.TestCase):
+class BoxModelInit(unittest.TestCase):
     def test_init0(self):
         self.assertRaises(ValueError, BoxModel.BaseBox)
 
     def test_init1(self):
-        r = 1
+        r = randint(0, 100)
         instance = BoxModel.BaseBox(r)
         self.assertEqual(instance.Left, r)
         self.assertEqual(instance.Top, r)
@@ -15,9 +15,36 @@ class BoxModelTestCase(unittest.TestCase):
         self.assertEqual(instance.Bottom, r)
 
     def test_init4(self):
-        l, r, t, b = 1, 2, 3, 4
+        l, r, t, b = randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 100)
         instance = BoxModel.BaseBox(l, t, r, b)
         self.assertEqual(instance.Left, l)
         self.assertEqual(instance.Top, t)
         self.assertEqual(instance.Right, r)
         self.assertEqual(instance.Bottom, b)
+
+class BoxModelProperties(unittest.TestCase):
+    def setUp(self):
+        self.instance = BoxModel.BaseBox(0)
+
+    def tearDown(self):
+        del self.instance
+
+    def test_left(self):
+        v = randint(0, 100)
+        self.instance.Left = v
+        self.assertEqual(self.instance.Left, v)
+
+    def test_right(self):
+        v = randint(0, 100)
+        self.instance.Right = v
+        self.assertEqual(self.instance.Right, v)
+
+    def test_top(self):
+        v = randint(0, 100)
+        self.instance.Top = v
+        self.assertEqual(self.instance.Top, v)
+
+    def test_bottom(self):
+        v = randint(0, 100)
+        self.instance.Bottom = v
+        self.assertEqual(self.instance.Bottom, v)
