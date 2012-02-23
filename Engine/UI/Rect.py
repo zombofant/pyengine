ed7c1e682231f944eafee65a17a554dda4c3dd29
Jonas Wielicki <j.wielicki@sotecware.net>
2012-02-23 04:48:06 +0100

    Made the Rect semantic
    It supports both access via X, Y, Width, Height and Left, Top, Right, Bottom.

--- Engine/UI/Rect.py
+++ Engine/UI/Rect.py
@@ -4,23 +4,35 @@ from our_future import *
 class Rect(object):
     @staticmethod
     def _checkDimension(value):
-        value = int(value)
         if value < 0:
             raise ValueError("Rect dimensions must be non negative.")
         return value
         
     def __init__(self, *args):
-        if len(args) == 0:
-            self._x, self._y, self._width, self._height = 0, 0, 0, 0
-        elif len(args) == 2:
+        """
+        Takes zero, two or four integer (castable) arguments.
+
+        If none are passed, the Rect gets initialized with all
+        components set to zero.
+
+        If two are passed, these are interpreted as X and Y coordinates.
+        Width and Height of the rect are still set to zero.
+
+        If four are passed, the first two are X and Y, while the other
+        two are interpreted as Right and Bottom.
+        """
+        self._x, self._y = 0, 0
+        self._right, self._bottom = 0, 0
+        self._width, self._height = 0, 0
+        if len(args) == 2:
             self._x, self._y = (int(x) for x in args)
-            self._width = 0
-            self._height = 0
+            self._right, self._bottom = 0, 0
+            self._width, self._height = 0, 0
         elif len(args) == 4:
-            self._x, self._y = (int(x) for x in args[:2])
-            self._width, self._height = (self._checkDimension(x) for x in args[2:])
-        else:
-            raise ValueError("Rect requires 0, 2 or 4 integer arguments")
+            # Lets do the checks inside the property setters
+            self.X, self.Y, self.Right, self.Bottom = args
+        elif len(args) != 0:
+            raise ValueError("Rect takes 0, 2 or 4 int arguments.")
 
     @property
     def X(self):
@@ -28,7 +40,11 @@ class Rect(object):
 
     @X.setter
     def X(self, value):
-        self._x = int(value)
+        value = int(value)
+        if self._x == value:
+            return
+        self._x = value
+        self._right = self._x + self._width
 
     @property
     def Y(self):
@@ -36,7 +52,39 @@ class Rect(object):
 
     @Y.setter
     def Y(self, value):
-        self._y = int(value)
+        value = int(value)
+        if self._y == value:
+            return
+        self._y = value
+        self._bottom = self._y + self._height
+
+    @property
+    def Left(self):
+        return self._x
+
+    @Left.setter
+    def Left(self, value):
+        value = int(value)
+        if self._x == value:
+            return
+        if value > self._right:
+            raise ValueError("Left must be smaller than or equal to right.")
+        self._x = value
+        self._width = self._right - self._x
+
+    @property
+    def Top(self):
+        return self._y
+
+    @Top.setter
+    def Top(self, value):
+        value = int(value)
+        if self._y == value:
+            return
+        if value > self._bottom:
+            raise ValueError("Top must be smaller than or equal to bottom.")
+        self._y = value
+        self._height = self._bottom - self._y
 
     @property
     def Width(self):
@@ -44,7 +92,11 @@ class Rect(object):
 
     @Width.setter
     def Width(self, value):
+        value = int(value)
+        if self._width == value:
+            return
         self._width = self._checkDimension(value)
+        self._right = self._x + self._width
 
     @property
     def Height(self):
@@ -52,5 +104,54 @@ class Rect(object):
 
     @Height.setter
     def Height(self, value):
-        self._heigth = self._checkDimension(value)
-    
+        value = int(value)
+        if self._height == value:
+            return
+        self._height = self._checkDimension(value)
+        self._bottom = self._y + self._height
+
+    @property
+    def Right(self):
+        return self._right
+
+    @Right.setter
+    def Right(self, value):
+        value = int(value)
+        if self._right == value:
+            return
+        if value < self._x:
+            raise ValueError("Right must be larger than or equal to left.")
+        self._right = value
+        self._width = self._right - self._x
+
+    @property
+    def Bottom(self):
+        return self._bottom
+
+    @Bottom.setter
+    def Bottom(self, value):
+        value = int(value)
+        if self._bottom == value:
+            return
+        if value < self._y:
+            raise ValueError("Bottom must be larger than or equal to top.")
+        self._bottom = value
+        self._height = self._bottom - self._y
+
+    def transpose(self, byX, byY):
+        self.X = self._x + byX
+        self.Y = sefl._y + byY
+
+    def __and__(self, other):
+        if not isinstance(other, Rect):
+            return NotImplemented
+        r1, b1 = self._x + self._width - 1, self._y + self._height - 1
+        r2, b2 = other._x + other._width - 1, other._x + other._height - 1
+        x, y = max(self._x, other._x), max(self._y, other._y)
+        return Rect(x, y, min(r1, r2) - x, min(b1, b2) - y)
+
+    def __contains__(self, other):
+        if not isinstance(other, Rect):
+            return NotImplemented
+            
+        
--- Engine/UI/test_Rect.py
+++ Engine/UI/test_Rect.py
@@ -2,63 +2,92 @@ import Rect
 import unittest
 from random import randint
 
-class RectInstanceTest(unittest.TestCase):
+class RectTest(unittest.TestCase):
+    def checkValuesXYWH(self, x, y, w, h):
+        self.assertEqual(self.instance.X, x)
+        self.assertEqual(self.instance.Y, y)
+        self.assertEqual(self.instance.Width, w)
+        self.assertEqual(self.instance.Height, h)
+        
+    def checkValuesXYRB(self, x, y, r, b):
+        self.assertEqual(self.instance.X, x)
+        self.assertEqual(self.instance.Y, y)
+        self.assertEqual(self.instance.Right, r)
+        self.assertEqual(self.instance.Bottom, b)
+
+class RectInstanceTest(RectTest):
     def setUp(self):
         self.instance = Rect.Rect()
 
     def tearDown(self):
         del self.instance
 
-class RectInit(unittest.TestCase):
-    def checkValues(self, x, y, w, h):
-        self.assertEqual(self.instance.X, x)
-        self.assertEqual(self.instance.Y, y)
-        self.assertEqual(self.instance.Width, w)
-        self.assertEqual(self.instance.Height, h)
-    
+class RectInit(RectTest):
     def test_init0(self):
         self.instance = Rect.Rect()
-        self.checkValues(0, 0, 0, 0)
+        self.checkValuesXYRB(0, 0, 0, 0)
 
     def test_init2(self):
         x = randint(0, 100)
         y = randint(0, 100) + x
         self.instance = Rect.Rect(x, y)
-        self.checkValues(x, y, 0, 0)
+        self.checkValuesXYRB(x, y, 0, 0)
 
     def test_init4(self):
         # guarantee that all values are random and non-equal
         x = randint(1, 100)
         y = randint(1, 100) + x
-        w = randint(1, 100) + y
-        h = randint(1, 100) + w
-        self.instance = Rect.Rect(x, y, w, h)
-        self.checkValues(x, y, w, h)
+        r = randint(1, 100) + x
+        b = randint(1, 100) + y
+        self.instance = Rect.Rect(x, y, r, b)
+        self.checkValuesXYRB(x, y, r, b)
 
     def tearDown(self):
         del self.instance
 
 class RectProperties(RectInstanceTest):
-    def test_left(self):
-        v = randint(0, 100)
-        self.instance.Left = v
-        self.assertEqual(self.instance.Left, v)
-
-    def test_right(self):
-        v = randint(0, 100)
-        self.instance.Right = v
+    def test_x(self):
+        v = randint(1, 100)
+        self.instance.X = v
+        self.assertEqual(self.instance.X, v)
         self.assertEqual(self.instance.Right, v)
 
-    def test_top(self):
-        v = randint(0, 100)
-        self.instance.Top = v
-        self.assertEqual(self.instance.Top, v)
-
-    def test_bottom(self):
-        v = randint(0, 100)
-        self.instance.Bottom = v
+    def test_y(self):
+        v = randint(1, 100)
+        self.instance.Y = v
+        self.assertEqual(self.instance.Y, v)
         self.assertEqual(self.instance.Bottom, v)
 
+    def test_rw(self):
+        w, x = randint(1, 100), randint(1, 100)
+        r = x + randint(1, 100)
+        self.instance.Width = w
+        self.assertEqual(self.instance.Width, w)
+        self.assertEqual(self.instance.Right, w)
+
+        self.instance.X = x
+        self.assertEqual(self.instance.Right, x + w)
+        self.assertEqual(self.instance.Width, w)
+
+        self.instance.Right = r
+        self.assertEqual(self.instance.Right, r)
+        self.assertEqual(self.instance.Width, r - x)
+
+    def test_bh(self):
+        h, y = randint(1, 100), randint(1, 100)
+        b = y + randint(1, 100)
+        self.instance.Height = h
+        self.assertEqual(self.instance.Height, h)
+        self.assertEqual(self.instance.Bottom, h)
+
+        self.instance.Y = y
+        self.assertEqual(self.instance.Bottom, y + h)
+        self.assertEqual(self.instance.Height, h)
+
+        self.instance.Bottom = b
+        self.assertEqual(self.instance.Bottom, b)
+        self.assertEqual(self.instance.Height, b - y)
+        
 class RectConstraints(RectInstanceTest):
     def setattrWrapper(self, attr, value):
         def c():
