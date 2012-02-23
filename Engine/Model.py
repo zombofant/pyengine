964aeb3b4f9a13ade72046ef0e489f161b081bbb
Rene Kuettner <rene@bitkanal.net>
2012-02-23 19:55:08 +0100

    Add basic Model classes (Model.py)
    * GLModel is a generic GL model class
    * OBJGLModel is a GLModel interface that can load wavefront obj files

---
+++ Engine/Model.py
@@ -0,0 +1,147 @@
+# File name: Model.py
+# This file is part of: pyuni
+#
+# LICENSE
+#
+# The contents of this file are subject to the Mozilla Public License
+# Version 1.1 (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+# the License for the specific language governing rights and limitations
+# under the License.
+#
+# Alternatively, the contents of this file may be used under the terms
+# of the GNU General Public license (the  "GPL License"), in which case
+# the provisions of GPL License are applicable instead of those above.
+#
+# FEEDBACK & QUESTIONS
+#
+# For feedback and questions about pyuni please e-mail one of the
+# authors named in the AUTHORS file.
+########################################################################
+from __future__ import unicode_literals, print_function, division
+from our_future import *
+from pyglet.gl import *
+
+"""
+The GLModel class stores 3D model data like vertices, normals,
+faces and texture coordinates, rotation, position, scale and so on.
+It also provides methods to modify and render the model using OpenGL.
+"""
+class GLModel(object):
+
+    def __init__(self):
+        self._clear()
+
+    def _clear(self):
+        self._vertices, self._faces, self_normals, self._faces = [], [], [], []
+        self._posX, self._posY, self._posZ = 0., 0., 0.
+        self._angleX, self._angleY, self._angleZ = 0., 0., 0.
+        self._scaleX, self._scaleY, self._scaleZ = 1., 1., 1.
+
+    def _transform(self):
+        glTranslatef(self._posX, self._posY, self._posZ)
+        glRotatef(self._angleX, 1., 0., 0.)
+        glRotatef(self._angleY, 0., 1., 0.)
+        glRotatef(self._angleZ, 0., 0., 1.)
+        glScalef(self._scaleX, self._scaleY, self._scaleZ)
+
+    def render(self):
+        # FIXME: implement VBOs
+        glPushMatrix()
+        self._transform()
+        for face in self.faces:
+            glBegin(GL_POLYGON)
+            glColor3f(1,0,0)
+            for ids in face:
+                if ids[1] is not None:
+                    texCoord = self.texCoords[ids[1]]
+                    glTexCoord2f(texCoord[0], texCoord[1])
+                if ids[2] is not None:
+                    normal = self.normals[ids[2]]
+                    glNormal3f(normal[0], normal[1], normal[2])
+                vertex = self.vertices[ids[0]]
+                glVertex3f(vertex[0], vertex[1], vertex[2])
+            glEnd()
+        glPopMatrix()
+
+    @property
+    def position(self):
+        return [self._posX, self._posY, self._posZ]
+
+    @position.setter
+    def position(self, value):
+        self._posX, self._posY, self._posZ = value
+
+    @property
+    def rotation(self):
+        return [self._angleX, self._angleY, self._angleZ]
+
+    @rotation.setter
+    def rotation(self, value):
+        self._angleX, self._angleY, self._angleZ = value
+
+    @property
+    def vertices(self):
+        return self._vertices
+
+    @property
+    def normals(self):
+        return self._normals
+
+    @property
+    def texcoords(self):
+        return self._texcoords
+
+    @property
+    def faces(self):
+        return self._faces
+
+
+"""
+The OBJGLModel provides a GLModel interface with support for loading
+3d geometry data from wavefront obj formatted files.
+See http://www.martinreddy.net/gfx/3d/OBJ.spec for a format specification.
+"""
+class OBJGLModel(GLModel):
+
+    def __init__(self, filename=None):
+        super(OBJGLModel, self).__init__()
+        if filename is not None:
+            self.loadFromFile(filename)
+
+    def loadFromFile(self, filename):
+        vertices, normals, texcoords, faces = [], [], [], []
+        with open(str(filename), 'r') as objf:
+            for line in objf:
+                if line[0] == '#' : continue
+                parts = line.strip().split(' ')
+                if parts[0] == 'v':
+                    vertices.append([float(x) for x in parts[1:]])
+                elif parts[0] == 'vn':
+                    normals.append([float(x) for x in parts[1:]])
+                elif parts[0] == 'vt':
+                    texcoords.append([float(x) for x in parts[1:]])
+                elif parts[0] == 'f':
+                    face = []
+                    for i in range(1,len(parts)):
+                        fcomp = []
+                        fcomps = parts[i].split('/')
+                        for j in range(0,3):
+                            if len(fcomps) <= j or fcomps[j] == '':
+                                fcomp.append(None)
+                            else:
+                                fcomp.append(int(fcomps[j])-1)
+                        face.append(fcomp)
+                    faces.append(face)
+                else:
+                    print("FIXME: Unhandled obj data: %s" % line)
+        print('GameModel loaded from %s' % filename)
+        self._vertices = vertices
+        self._normals = normals
+        self._texcoords = texcoords
+        self._faces = faces
+
