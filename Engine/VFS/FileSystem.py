335538b92e5a1fe107e0c88b1311152909d50386
Rene Kuettner <rene@bitkanal.net>
2012-02-26 16:39:07 +0100

    Some minor code fixes.

--- Engine/VFS/FileSystem.py
+++ Engine/VFS/FileSystem.py
@@ -130,8 +130,8 @@ class FileSystem(object):
 class XDGFileSystem(FileSystem):
     def _setupMounts(self, mountPoint, globalDirs, homeDir):
         for dir in globalDirs:
-            mount(mountPoint, MountDirectory(dir, readOnly=True), MountPriorities.FileSystem)
-        mount(mountPoint, MountDirectory(homeDir, readOnly=False), MountPriorities.Important)
+            self.mount(mountPoint, MountDirectory(dir, readOnly=True), MountPriority.FileSystem)
+        self.mount(mountPoint, MountDirectory(homeDir, readOnly=False), MountPriority.Important)
     
     def __init__(self, appDirName, dataMountPoint="/data", configMountPoint="/config", **kwargs):
         super(XDGFileSystem, self).__init__(**kwargs)
