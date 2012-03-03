e1a40c04cf681d9d1267ab258dbc95aee003a33c
Jonas Wielicki <j.wielicki@sotecware.net>
2012-03-03 22:24:04 +0100

    Some first drafts

--- client-host/WindowInterface/X11/Display.cpp
+++ client-host/WindowInterface/X11/Display.cpp
@@ -1,5 +1,5 @@
 /**********************************************************************
-File name: pyuni-client.cpp
+File name: Display.cpp
 This file is part of: Pythonic Universe
 
 LICENSE
@@ -23,48 +23,3 @@ FEEDBACK & QUESTIONS
 For feedback and questions about pyuni please e-mail one of the authors
 named in the AUTHORS file.
 **********************************************************************/
-
-
-
-
-#include <iostream>
-//#include <boost/python.hpp>
-
-using namespace std;
-/*using namespace boost::python;
-
-struct World
-{
-    void set(std::string msg) { this->msg = msg; }
-    std::string greet() { return msg; }
-    std::string msg;
-};
-
-BOOST_PYTHON_MODULE(hello)
-{
-    class_<World>("World")
-        .def("greet", &World::greet)
-        .def("set", &World::set)
-    ;
-}*/
-
-int main() {
-    
-    
-    /*PyImport_AppendInittab("hello", &inithello);
-    Py_Initialize();
-    object main = import("__main__");
-    object main_namespace = main.attr("__dict__");
-
-    exec(
-"\
-import hello;\
-print(\"Hello World from inside-python print()!\");\
-test = hello.World();\
-test.set(\"Hello World from hello.World!\");\
-print(test.greet());\
-",
-                          main_namespace);*/
-
-    
-}
