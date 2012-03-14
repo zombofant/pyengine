423db70d45ad42f55b369556769288dc41e21211
Jonas Wielicki <j.wielicki@sotecware.net>
2012-03-14 14:35:28 +0100

    Implemented VertexFormat constructor wrapper

--- CUni/GL.py
+++ CUni/GL.py
@@ -23,3 +23,50 @@
 # authors named in the AUTHORS file.
 ########################################################################
 from _cuni_gl import *
+
+def __testSizeRange(size, min, max, type):
+    if size > max or size < min:
+        raise ValueError("Size must be in [{0}..{1}] for {2}. Got {3}".format(min, max, type, size))
+    return size
+
+def __testIndexRange(idx, min, max, type):
+    idx = int(idx)
+    if idx > max or idx < min:
+        raise ValueError("Index must be in [{0}..{1}] for {2}. Got {3}".format(min, max, type, idx))
+    return idx
+
+__VertexFormat = VertexFormat
+
+def VertexFormat(fmtSpecifier):
+    nVertex, nColour = 0, 0
+    nTexCoord = [0, 0, 0, 0]
+    nAttrib = [0, 0, 0, 0]
+    hasNormal = False
+
+    if not isinstance(fmtSpecifier, (unicode, str)):
+        segmentIterable = fmtSpecifier
+    else:
+        segmentIterable = fmtSpecifier.split(";")
+    segments = [segment.split(':') for segment in segmentIterable]
+    for type, size in ((type.strip(), size.strip()) for type, size in segments):
+        if len(type) == 0:
+            raise ValueError("Invalid type specifier: {0}".format(type))
+        size = int(size)
+
+        attrib = type[0]
+        if attrib == "v":
+            nVertex = __testSizeRange(size, 2, 4, attrib)
+        elif attrib == "c":
+            nColour = __testSizeRange(size, 3, 4, attrib)
+        elif attrib == "n":
+            __testSizeRange(size, 3, 3, attrib)
+            hasNormal = True
+        elif attrib == "t":
+            idx = __testIndexRange(type[1:], 0, len(nTexCoord), attrib)
+            nTexCoord[idx] = __testSizeRange(size, 1, 4, type)
+        elif attrib == "g" or attrib == "a":
+            idx = __testIndexRange(type[1:], 0, len(nAttrib), attrib)
+            nAttrib[idx] = __testSizeRange(size, 1, 4, type)
+
+    args = [nVertex, nColour] + nTexCoord + [hasNormal] + nAttrib;
+    return __VertexFormat(*args)
