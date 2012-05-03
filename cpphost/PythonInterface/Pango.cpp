69c3499ca88d2185cba83015513355c2917e0b32
Jonas Wielicki <j.wielicki@sotecware.net>
2012-05-03 17:20:06 +0200

    Extend pango interface by essential methods
    * updateContext to update the PangoContext for use with cairo
    * contextChanged to notify layouts about the update
    * setMarkup to set formatted text
    * wrappers around setText/getText to accept unicode

--- cpphost/PythonInterface/Pango.cpp
+++ cpphost/PythonInterface/Pango.cpp
@@ -1,3 +1,28 @@
+/**********************************************************************
+File name: Pango.cpp
+This file is part of: Pythonic Universe
+
+LICENSE
+
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS"
+basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+License for the specific language governing rights and limitations under
+the License.
+
+Alternatively, the contents of this file may be used under the terms of
+the GNU General Public license (the  "GPL License"), in which case  the
+provisions of GPL License are applicable instead of those above.
+
+FEEDBACK & QUESTIONS
+
+For feedback and questions about pyuni please e-mail one of the authors
+named in the AUTHORS file.
+**********************************************************************/
 #include "Pango.hpp"
 
 #include <boost/python.hpp>
@@ -38,17 +63,31 @@ void PangoCairoContext::showLayout(PangoLayoutHandle layout)
     pango_cairo_show_layout(_cairoCtx, layout->getPangoLayout());
 }
 
+void PangoCairoContext::updateContext()
+{
+    pango_cairo_update_context(_cairoCtx, _ctx);
+}
+
 /* PyUni::PangoLayout */
 
 PangoLayout::PangoLayout(PangoCairoContextHandle ctx):
     _layout(pango_layout_new(ctx->getPangoContext()))
 {
-
+    PangoFontDescription *descr = pango_font_description_new();
+    pango_font_description_set_family(descr, "Cantarell");
+    pango_font_description_set_size(descr, 10*PANGO_SCALE);
+    pango_layout_set_font_description(_layout, descr);
+    pango_font_description_free(descr);
 }
 
 PangoLayout::~PangoLayout()
 {
     g_object_unref(_layout);
+}
+
+void PangoLayout::contextChanged()
+{
+    pango_layout_context_changed(_layout);
 }
 
 PangoEllipsizeMode PangoLayout::getEllipsize()
@@ -93,30 +132,33 @@ bool PangoLayout::isEllipsized()
 
 void PangoLayout::setEllipsize(PangoEllipsizeMode ellipsize)
 {
-    return pango_layout_set_ellipsize(_layout, ellipsize);
+    pango_layout_set_ellipsize(_layout, ellipsize);
 }
 
 void PangoLayout::setHeight(int height)
 {
-    return pango_layout_set_height(_layout, height);
+    pango_layout_set_height(_layout, height);
+}
+
+void PangoLayout::setMarkup(const char *markup, int length)
+{
+    pango_layout_set_markup(_layout, markup, length);
 }
 
 void PangoLayout::setText(const char *text, int length)
 {
-    return pango_layout_set_text(_layout, text, length);
+    pango_layout_set_text(_layout, text, length);
 }
 
 void PangoLayout::setWidth(int width)
 {
-    return pango_layout_set_width(_layout, width);
+    pango_layout_set_width(_layout, width);
 }
 
 void PangoLayout::setWrap(PangoWrapMode mode)
 {
-    return pango_layout_set_wrap(_layout, mode);
+    pango_layout_set_wrap(_layout, mode);
 }
-
-
 
 using namespace boost::python;
 
@@ -130,18 +172,72 @@ PangoCairoContextHandle bp_createPangoContext(PyObject *contextObj)
     return PangoCairoContextHandle(new PangoCairoContext(context));
 }
 
+PyObject *bp_PangoLayout_getText(PangoLayoutHandle layout)
+{
+    const char *text = layout->getText();
+    Py_ssize_t len = strlen(text);
+    return PyUnicode_DecodeUTF8(text, len, 0);
+}
+
+PyObject *bp_extractPangoStr(PyObject *text, const char **data, int *len)
+{
+    PyObject *utf8Text = text;
+    if (PyString_Check(text)) {
+        // we hope its utf8 here...
+        Py_INCREF(utf8Text);
+    }
+    else if (!PyUnicode_Check(text)) {
+        PyErr_SetString(PyExc_TypeError, "Need unicode or (utf8 encoded) str");
+        throw error_already_set();
+    }
+    utf8Text = PyUnicode_AsUTF8String(text);
+    if (!utf8Text) {
+        throw error_already_set();
+    }
+
+    *data = PyString_AS_STRING(utf8Text);
+    *len = PyString_GET_SIZE(utf8Text);
+    return utf8Text;
+}
+
+void bp_PangoLayout_setText(PangoLayoutHandle layout, PyObject *rawText)
+{
+    const char *data = 0;
+    int len = 0;
+    PyObject *text = bp_extractPangoStr(rawText, &data, &len);
+    // FIXME: length check here.
+    layout->setText(data, len);
+
+    Py_DECREF(text);
+}
+
+void bp_PangoLayout_setMarkup(PangoLayoutHandle layout, PyObject *rawText)
+{
+    const char *data = 0;
+    int len = 0;
+    PyObject *text = bp_extractPangoStr(rawText, &data, &len);
+    // FIXME: length check here.
+    layout->setMarkup(data, len);
+
+    Py_DECREF(text);
+}
+
+
 BOOST_PYTHON_MODULE(_cuni_pango)
 {
     class_<PangoCairoContext, PangoCairoContextHandle>("PangoCairoContext", no_init)
         .def("__init__", make_constructor(&bp_createPangoContext))
         .def("layoutPath", &PangoCairoContext::layoutPath)
         .def("showLayout", &PangoCairoContext::showLayout)
+        .def("updateContext", &PangoCairoContext::updateContext)
     ;
 
     class_<PangoLayout, PangoLayoutHandle>("PangoLayout", init<PangoCairoContextHandle>())
+        .def("contextChanged", &PangoLayout::contextChanged)
+        .def("setMarkup", &bp_PangoLayout_setMarkup)
         .add_property("Ellipsize", &PangoLayout::getEllipsize, &PangoLayout::setEllipsize)
         .add_property("Height", &PangoLayout::getHeight, &PangoLayout::setHeight)
-        .add_property("Text", &PangoLayout::getText, &PangoLayout::setText)
+        .add_property("Text", bp_PangoLayout_getText, bp_PangoLayout_setText)
         .add_property("Width", &PangoLayout::getWidth, &PangoLayout::setWidth)
         .add_property("Wrap", &PangoLayout::getWrap, &PangoLayout::setWrap)
         .add_property("IsEllipsized", &PangoLayout::isEllipsized)
@@ -149,7 +245,7 @@ BOOST_PYTHON_MODULE(_cuni_pango)
     ;
 }
 
-void addPangoToInitTab()
+void addPangoToInittab()
 {
     PyImport_AppendInittab("_cuni_pango", &init_cuni_pango);
 }
--- cpphost/PythonInterface/Pango.hpp
+++ cpphost/PythonInterface/Pango.hpp
@@ -1,3 +1,28 @@
+/**********************************************************************
+File name: Pango.hpp
+This file is part of: Pythonic Universe
+
+LICENSE
+
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS"
+basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+License for the specific language governing rights and limitations under
+the License.
+
+Alternatively, the contents of this file may be used under the terms of
+the GNU General Public license (the  "GPL License"), in which case  the
+provisions of GPL License are applicable instead of those above.
+
+FEEDBACK & QUESTIONS
+
+For feedback and questions about pyuni please e-mail one of the authors
+named in the AUTHORS file.
+**********************************************************************/
 #ifndef _PYUNI_PYTHON_INTERFACE_PANGO
 #define _PYUNI_PYTHON_INTERFACE_PANGO
 
@@ -22,6 +47,7 @@ class PangoCairoContext {
         PangoContext *getPangoContext();
         void layoutPath(PangoLayoutHandle layout);
         void showLayout(PangoLayoutHandle layout);
+        void updateContext();
 };
 
 typedef boost::shared_ptr<PangoCairoContext> PangoCairoContextHandle;
@@ -33,6 +59,8 @@ class PangoLayout {
     private:
         ::PangoLayout *_layout;
     public:
+        void contextChanged();
+    
         PangoEllipsizeMode getEllipsize();
         int getHeight();
         ::PangoLayout *getPangoLayout();
@@ -45,12 +73,13 @@ class PangoLayout {
 
         void setEllipsize(PangoEllipsizeMode ellipsize);
         void setHeight(int height);
+        void setMarkup(const char *markup, int length);
         void setText(const char *text, int length);
         void setWidth(int width);
         void setWrap(PangoWrapMode mode);
 };
 
-void addPangoToInitTab();
+void addPangoToInittab();
 
 }
 
