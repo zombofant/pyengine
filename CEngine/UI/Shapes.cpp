b15e5ffa7e3589047aca10d072e9ac81b597eedf
Jonas Wielicki <j.wielicki@sotecware.net>
2013-03-15 12:56:18 +0100

    Start c++ version of UI -- port basic shape classes

---
+++ CEngine/UI/Shapes.cpp
@@ -0,0 +1,384 @@
+#include "Shapes.hpp"
+
+namespace PyEngine {
+
+/* PyEngine::BoxError */
+
+BoxError::BoxError(const std::string& what_arg):
+    std::invalid_argument(what_arg)
+{
+
+}
+
+BoxError::BoxError(const char* what_arg):
+    std::invalid_argument(what_arg)
+{
+
+}
+
+/* PyEngine::RectError */
+
+RectError::RectError(const std::string& what_arg):
+    std::invalid_argument(what_arg)
+{
+
+}
+
+RectError::RectError(const char* what_arg):
+    std::invalid_argument(what_arg)
+{
+
+}
+
+/* PyEngine::Box */
+
+Box::Box():
+    _left(0), _top(0), _right(0), _bottom(0)
+{
+
+}
+
+Box::Box(coord_int_t value):
+    _left(value),
+    _top(value),
+    _right(value),
+    _bottom(value)
+{
+
+}
+
+Box::Box(coord_int_t left, coord_int_t right,
+         coord_int_t top, coord_int_t bottom):
+    _left(left),
+    _top(top),
+    _right(right),
+    _bottom(bottom)
+{
+
+}
+
+Box::Box(const Box& ref):
+    _left(ref._left),
+    _top(ref._top),
+    _right(ref._right),
+    _bottom(ref._bottom)
+{
+
+}
+
+Box& Box::operator= (const Box& ref)
+{
+    _left = ref._left;
+    _top = ref._top;
+    _right = ref._right;
+    _bottom = ref._bottom;
+    return *this;
+}
+
+Box::~Box()
+{
+
+}
+
+void Box::_check_value(const coord_int_t value) const
+{
+    if (value == AUTO) {
+        throw BoxError("Non-margin boxes do not support AUTO value.");
+    }
+    if (value < 0) {
+        throw BoxError("Non-margin boxes do not allow negative values.");
+    }
+}
+
+void Box::assign(const Box& ref)
+{
+    *this = ref;
+}
+
+inline bool Box::operator==(const Box& b) const
+{
+    return ((_left == b._left) &&
+            (_top == b._top) &&
+            (_right == b._right) &&
+            (_bottom == b._bottom));
+}
+
+inline bool Box::operator!=(const Box& b) const
+{
+    return !(operator==(b));
+}
+
+/* PyEngine::Rect */
+
+Rect::Rect():
+    _left(0), _top(0), _width(-1), _height(-1)
+{
+
+}
+
+Rect::Rect(coord_int_t top, coord_int_t left):
+    _left(left),
+    _top(top),
+    _width(0),
+    _height(0)
+{
+
+}
+
+Rect::Rect(coord_int_t left, coord_int_t top,
+           coord_int_t right, coord_int_t bottom):
+    _left(left),
+    _top(top),
+    _width(0),
+    _height(0)
+{
+    set_right(right);
+    set_bottom(bottom);
+}
+
+Rect::Rect(const Rect &ref):
+    _left(ref._left),
+    _top(ref._top),
+    _width(ref._width),
+    _height(ref._height)
+{
+
+}
+
+Rect& Rect::operator= (const Rect &ref)
+{
+    _left = ref._left;
+    _top = ref._top;
+    _width = ref._width;
+    _height = ref._height;
+    return *this;
+}
+
+Rect::~Rect()
+{
+
+}
+
+void Rect::set_x(const coord_int_t value)
+{
+    _left = value;
+}
+
+void Rect::set_y(const coord_int_t value)
+{
+    _top = value;
+}
+
+void Rect::set_width(const coord_int_t value)
+{
+    check_dimension(value, "width");
+    _width = value;
+}
+
+void Rect::set_height(const coord_int_t value)
+{
+    check_dimension(value, "height");
+    _height = value;
+}
+
+void Rect::set_left(const coord_int_t value)
+{
+    if (value > get_right()) {
+        throw RectError("left must be less than or equal to right.");
+    }
+    _left = value;
+}
+
+void Rect::set_top(const coord_int_t value)
+{
+    if (value > get_bottom()) {
+        throw RectError("top must be less than or equal to bottom.");
+    }
+    _top = value;
+}
+
+void Rect::set_right(const coord_int_t value)
+{
+    if (value < _left) {
+        throw RectError("right must be greater than or equal to left.");
+    }
+    _width = value - _left;
+}
+
+void Rect::set_bottom(const coord_int_t value)
+{
+    if (value < _top) {
+        throw RectError("bottom must be greater than or equal to top.");
+    }
+    _height = value - _top;
+}
+
+coord_int_t Rect::area() const
+{
+    if (is_a_rect()) {
+        return 0;
+    }
+    return _width * _height;
+}
+
+void Rect::assign(const Rect& ref)
+{
+    *this = ref;
+}
+
+void Rect::translate(const coord_int_t dx, const coord_int_t dy)
+{
+    _left += dx;
+    _top += dy;
+}
+
+inline bool Rect::operator==(const Rect& b) const
+{
+    return (_left == b._left) &&
+        (_top == b._top) &&
+        (_width == b._width) &&
+        (_height == b._height);
+}
+
+inline bool Rect::operator!=(const Rect& b) const
+{
+    return !(operator==(b));
+}
+
+Rect& Rect::operator&=(const Rect& b)
+{
+    if (!is_a_rect() || !b.is_a_rect()) {
+        return *this;
+    }
+    const coord_int_t new_left = std::max(_left, b._left);
+    const coord_int_t new_top = std::max(_top, b._top);
+    const coord_int_t new_right = std::min(get_right(), b.get_right());
+    const coord_int_t new_bottom = std::min(get_bottom(), b.get_bottom());
+    if (new_left > new_right || new_top > new_bottom) {
+        _width = -1;
+        _height = -1;
+    }
+    _left = new_left;
+    _top = new_top;
+    _width = new_right - new_left;
+    _height = new_bottom - new_top;
+    return *this;
+}
+
+Rect Rect::operator&(const Rect& b) const
+{
+    Rect result = Rect(*this);
+    result &= b;
+    return std::move(result);
+}
+
+Rect& Rect::operator|=(const Rect& b)
+{
+    if (!is_a_rect() || !b.is_a_rect()) {
+        return *this;
+    }
+
+    if (_left == b._left && _width == b._width) {
+        const coord_int_t me_bottom = get_bottom();
+        const coord_int_t b_bottom = b.get_bottom();
+        if (b._top <= me_bottom && _top <= b_bottom) {
+            _top = std::min(_top, b._top);
+            _height = std::max(me_bottom, b_bottom) - _top;
+        } else {
+            throw RectError("Cannot combine rects (not adjacent).");
+        }
+    } else if (_top == b._top && _height == b._height) {
+        const coord_int_t me_right = get_right();
+        const coord_int_t b_right = b.get_right();
+        if (b._left <= me_right && _left <= b_right) {
+            _left = std::min(_left, b._left);
+            _width = std::max(me_right, b_right) - _left;
+        } else {
+            throw RectError("Cannot combine rects (not adjacent).");
+        }
+
+    } else {
+        throw RectError("Cannot combine rects (not aligned).");
+    }
+    return *this;
+}
+
+Rect Rect::operator|(const Rect& b) const
+{
+    Rect result = Rect(*this);
+    result |= b;
+    return result;
+}
+
+bool Rect::operator<=(const Rect& b) const
+{
+    if (!b.is_a_rect()) {
+        return true;
+    }
+    if (!is_a_rect()) {
+        return false;
+    }
+
+    const coord_int_t me_right = get_right();
+    const coord_int_t me_bottom = get_bottom();
+
+    const coord_int_t b_right = b.get_right();
+    const coord_int_t b_bottom = b.get_bottom();
+
+    return ((_left <= b._left) &&
+            (_top <= b._top) &&
+            (me_right >= b_right) &&
+            (me_bottom >= b_bottom));
+}
+
+bool Rect::operator<(const Rect& b) const
+{
+    return (*this <= b) && (*this != b);
+}
+
+/* PyEngine::Margin */
+
+Margin::Margin():
+    Box::Box()
+{
+
+}
+
+Margin::Margin(coord_int_t value):
+    Box::Box(value)
+{
+
+}
+
+Margin::Margin(coord_int_t left, coord_int_t top,
+               coord_int_t right, coord_int_t bottom):
+    Box::Box(left, top, right, bottom)
+{
+
+}
+
+Margin::Margin(const Box& ref):
+    Box::Box(ref)
+{
+
+}
+
+Margin::Margin(const Margin& ref):
+    Box::Box(ref)
+{
+
+}
+
+Margin& Margin::operator= (const Margin& ref)
+{
+    this->Box::operator=(ref);
+    return *this;
+}
+
+Margin& Margin::operator= (const Box& ref)
+{
+    this->Box::operator=(ref);
+    return *this;
+}
+
+}
---
+++ CEngine/UI/Shapes.hpp
@@ -0,0 +1,179 @@
+#ifndef _PYE_UI_SHAPES_H
+#define _PYE_UI_SHAPES_H
+
+#include <stdexcept>
+#include <limits>
+
+namespace PyEngine {
+
+typedef int coord_int_t;
+
+const coord_int_t AUTO = std::numeric_limits<coord_int_t>::min();
+
+class BoxError: public std::invalid_argument
+{
+public:
+    BoxError(const std::string& what_arg);
+    BoxError(const char* what_arg);
+};
+
+class RectError: public std::invalid_argument
+{
+public:
+    RectError(const std::string& what_arg);
+    RectError(const char* what_arg);
+};
+
+class Box
+{
+public:
+    Box();
+    Box(coord_int_t value);
+    Box(coord_int_t left, coord_int_t right,
+        coord_int_t top, coord_int_t bottom);
+    Box(const Box& ref);
+    Box& operator= (const Box& ref);
+    ~Box();
+private:
+    coord_int_t _left, _top, _right, _bottom;
+protected:
+    virtual void _check_value(const coord_int_t value) const;
+public:
+    inline void set_left(const coord_int_t value) {
+        _check_value(value);
+        _left = value;
+    };
+    inline void set_top(const coord_int_t value) {
+        _check_value(value);
+        _top = value;
+    };
+    inline void set_right(const coord_int_t value) {
+        _check_value(value);
+        _right = value;
+    };
+    inline void set_bottom(const coord_int_t value) {
+        _check_value(value);
+        _bottom = value;
+    };
+
+    inline coord_int_t get_left() const {
+        return _left;
+    };
+    inline coord_int_t get_top() const {
+        return _top;
+    };
+    inline coord_int_t get_right() const {
+        return _right;
+    };
+    inline coord_int_t get_bottom() const {
+        return _bottom;
+    };
+
+    inline coord_int_t get_horizontal() const {
+        return get_left() + get_right();
+    };
+    inline coord_int_t get_vertical() const {
+        return get_top() + get_bottom();
+    };
+public:
+    void assign(const Box& ref);
+public:
+    inline bool operator==(const Box& b) const;
+    inline bool operator!=(const Box& b) const;
+};
+
+class Rect
+{
+public:
+    Rect();
+    Rect(coord_int_t left, coord_int_t top);
+    Rect(coord_int_t left, coord_int_t top, coord_int_t right, coord_int_t bottom);
+    Rect(const Rect& ref);
+    Rect& operator= (const Rect& ref);
+    ~Rect();
+private:
+    coord_int_t _left, _top;
+    coord_int_t _width, _height;
+protected:
+    inline void check_dimension(const coord_int_t value,
+                                const std::string which) {
+        if (value < 0) {
+            throw RectError(which + " must be non-negative.");
+        }
+    };
+public:
+    inline bool is_a_rect() const {
+        return (_width >= 0) && (_height >= 0);
+    };
+
+    void set_x(const coord_int_t value);
+    void set_y(const coord_int_t value);
+    void set_width(const coord_int_t value);
+    void set_height(const coord_int_t value);
+
+    void set_left(const coord_int_t value);
+    void set_top(const coord_int_t value);
+    void set_right(const coord_int_t value);
+    void set_bottom(const coord_int_t value);
+
+    inline coord_int_t get_x() const {
+        return _left;
+    };
+    inline coord_int_t get_y() const {
+        return _top;
+    };
+    inline coord_int_t get_width() const {
+        return _width;
+    };
+    inline coord_int_t get_height() const {
+        return _height;
+    };
+
+    inline coord_int_t get_left() const {
+        return _left;
+    };
+    inline coord_int_t get_top() const {
+        return _top;
+    };
+    inline coord_int_t get_right() const {
+        return _left + _width;
+    };
+    inline coord_int_t get_bottom() const {
+        return _top + _height;
+    };
+public:
+    coord_int_t area() const;
+    void assign(const Rect& ref);
+    void translate(const coord_int_t dx, const coord_int_t dy);
+public:
+    inline bool operator==(const Rect& b) const;
+    inline bool operator!=(const Rect& b) const;
+    Rect& operator&=(const Rect& b);
+    Rect operator&(const Rect& b) const;
+    Rect& operator|=(const Rect& b);
+    Rect operator|(const Rect& b) const;
+    bool operator<=(const Rect& b) const;
+    bool operator<(const Rect& b) const;
+};
+
+class Margin: public Box
+{
+public:
+    Margin();
+    Margin(coord_int_t value);
+    Margin(coord_int_t left, coord_int_t top,
+           coord_int_t right, coord_int_t bottom);
+    Margin(const Box& ref);
+    Margin(const Margin& ref);
+    Margin& operator= (const Margin& ref);
+    Margin& operator= (const Box& ref);
+protected:
+    virtual void _check_value(const coord_int_t value) const;
+public:
+    void deautoify(const Rect& obj, const Rect &outer);
+};
+
+}
+
+
+#endif
